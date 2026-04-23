4f91fa3629f4c1794c08ac5c5e463863d66057c4
Tinashe <tmanchaz@gmail.com>
2026-04-23 12:10:06 +0200

    Add search functionality

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,15 +1,28 @@
-import Navbar from "../components/common/Navbar";
-import Footer from "../components/common/Footer";
+import { useState } from "react";
 import ProductGrid from "../components/products/ProductGrid";
 import products from "../data/mockProducts";
+import SearchBar from "../components/products/SearchBar";
+
 
 export default function ProductsPage() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  
   return (
-    <>
+    <div>
       <div className="page-container">
-        <h1 style={{ textAlign: "center" }}>Products</h1>
-        <ProductGrid products={products} />
+        <h1>Products</h1>
+        
+        <SearchBar value={searchTerm} onChange={setSearchTerm} />
+        
+        {filteredProducts.length === 0 && (
+          <p>No products found</p> 
+        )}
+
+        <ProductGrid products={filteredProducts} />
       </div>
-    </>
+    </div>
   );
 }
